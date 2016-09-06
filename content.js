1f6b6c39cd026c895e98f082df25a10af83d4cab
Sean Norton <NortonSeanM@gmail.com>
2016-09-05 22:33:38 -0500

    Perfect fucking  amounts of harambe

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
-$('img').hover(function(){
-  var src = $(this).attr('src', "img.jpg");
-  console.log(src);
+var images = document.getElementsByTagName("img");
+var perfectAmount = Math.floor(images.length / 10 ) + 1
+
+for(var i = 0; i < perfectAmount ; i++) {
+  var rand = Math.floor(Math.random() * images.length)
+  images[rand].className += " harambelives";
+}
+
+$(".harambelives").mouseover(function(){
+  var src = $(this).attr('src');
+  $(this).attr('src', "https://raw.githubusercontent.com/SeanMNorton/harambe-chrome-extension/master/img.jpg")
 })
